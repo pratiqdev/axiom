aeb0680a739fd386866f0793a1dc03b8a12dcf7a
Shlep7 <53705976+Shlep7@users.noreply.github.com>
2021-02-03 15:42:59 -0500

    Maybe time for mdx??

---
+++ src/pages/MdArticles.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
+
+const MdArticles = () => {
+    return (
+        <ReactMarkdown>
+
+        </ReactMarkdown>
+    )
+}
